7d9de0cbee1c0a386956749c96c294b1a5b3181a
NikitaBelqa <Nik.belyakovich@yandex.ru>
2025-12-01 12:49:46 +0300

    Continue working 01.12

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ register = [0] * 64
 memory = [0] * 1024
 
 
-comands = {"45": lambda num, ind: register.__setitem__(ind, num),
-           "61": lambda target_ind, source: register.__setitem__(source, memory[register[target_ind]]),
-           "34": lambda reg_ind, mem_ind: memory.__setitem__(mem_ind, register[reg_ind]),
-           "5": lambda reg_ind, target_reg_ind: memory.__setitem__(register[target_reg_ind], (memory[register[reg_ind]] ** 1/2))}
+comands = {45: lambda num, ind: register.__setitem__(ind, num),
+           61: lambda target_ind, source: register.__setitem__(source, memory[register[target_ind]]),
+           34: lambda reg_ind, mem_ind: memory.__setitem__(mem_ind, register[reg_ind]),
+           5: lambda reg_ind, target_reg_ind: memory.__setitem__(register[target_reg_ind], (memory[register[reg_ind]] ** 1/2))}
 
 
 while True:
@@ -16,6 +16,6 @@ while True:
         print("register =", register)
         print("memory =", memory)
     else:
-        a, b, c = cmd.split(" ")
-        comands[a](int(b), int(c))
+        a, b, c = map(int, cmd.split(" "))
+        comands[a](b, c)
     
